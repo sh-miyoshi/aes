d2bcd9881824952a504fd5dc0079a1b6a5492624
Shunsuke Miyoshi <swfft814@gmail.com>
2019-10-13 17:15:28 +0900

    add base template

--- main.cpp
+++ main.cpp
@@ -6,22 +6,31 @@ int main() {
     unsigned char key[16] = {
         0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
     };
-    std::string planeText = "secret texts";
-    std::string encMsg;
+    std::string plainText = "This is a secret texts which you want to encrypt.";
+    std::string encMsg, decMsg;
 
     aes::AES::GenerateIV(iv, aes::AES_CTR);
+
+    // printf("iv: (");
+    // for (int i = 0; i < 16; i++) {
+    //     printf("%d", iv[i]);
+    //     if (i < 15) {
+    //         printf(", ");
+    //     }
+    // }
+    // puts(")");
+
     aes::AES handler(aes::AES_CTR, key, 128, iv);
-    aes::Error err = handler.Encrypt(planeText, &encMsg);
+    aes::Error err = handler.Encrypt(plainText, &encMsg);
     if (!err.success) {
-        printf("Failed to encypt: %s\n", err.message.c_str());
+        printf("Failed to encrypt: %s\n", err.message.c_str());
+    }
+    err = handler.Decrypt(encMsg, &decMsg);
+    if (!err.success) {
+        printf("Failed to decrypt: %s\n", err.message.c_str());
     }
 
-    printf("iv: (");
-    for (int i = 0; i < 16; i++) {
-        printf("%d", iv[i]);
-        if (i < 15) {
-            printf(", ");
-        }
-    }
-    puts(")");
+    printf("plain  : %s\n", plainText.c_str());
+    printf("encrypt: %s\n", encMsg.c_str());
+    printf("decrypt: %s\n", decMsg.c_str());
 }
