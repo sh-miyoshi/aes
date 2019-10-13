244090923bbe61a8fc9eb79ac7b4600bdf325d67
Shunsuke Miyoshi <swfft814@gmail.com>
2019-10-13 16:35:12 +0900

    add init process

--- aes.h
+++ aes.h
@@ -1,73 +1,85 @@
 #pragma once
 
-#include <string>
 #include <wmmintrin.h>
+#include <string>
 
-//#define USE_AES_NI
+// Please set USE_AES_NI to 1, if you want to use hardware accelerator.
+#define USE_AES_NI 1
+// #define USE_AES_NI 0
 
-class AES{
-public:
-	enum PaddingMode{
-		PADDING_ZERO,
-		PADDING_PKCS_5
-	};
+// FILE_READ_SIZE is a read byte size from file at once
+#define FILE_READ_SIZE 65536
 
-	typedef struct{
-		bool success;
-		std::string message;
-	}Error_t;
-private:
-	static const int MAX_NR=14;// max no of rounds
-	static const int FILE_READ_SIZE=65536;
-
-	unsigned int Nr;// number of rounds
-#ifdef USE_AES_NI
-	__m128i enc_key[MAX_NR+2],dec_key[MAX_NR+2];
-	__m128i iv;// initialization vector
-
-	__m128i AES_128_ASSIST(__m128i temp1,__m128i temp2);
-	void AES_192_ASSIST(__m128i &temp1,__m128i &temp2,__m128i &temp3);
-	void AES_256_ASSIST_1(__m128i &temp1,__m128i &temp2);
-	void AES_256_ASSIST_2(__m128i &temp1,__m128i &temp3);
-	void AES_128_Key_Expansion(__m128i *key,const unsigned char *user_key);
-	void AES_192_Key_Expansion(__m128i *key,const unsigned char *user_key);
-	void AES_256_Key_Expansion(__m128i *key,const unsigned char *user_key);
-#else
-	unsigned char iv[16];// initialize vector
-	unsigned char roundKey[16*(MAX_NR+1)];
-
-	inline void ExtMul(unsigned char &x,unsigned char data,int n);
-	void SubWord(unsigned char *w);
-	void RotWord(unsigned char *w);
-	void KeyExpansion(const unsigned char *userKey,int wordKeyLength);
-
-	inline void SubBytes(unsigned char *data);
-	inline void ShiftRows(unsigned char *data);
-	inline void MixColumns(unsigned char *data);
-	inline void InvSubBytes(unsigned char *data);
-	inline void InvShiftRows(unsigned char *data);
-	inline void InvMixColumns(unsigned char *data);
-	inline void AddRoundKey(unsigned char *data,int n);
-#endif
-
-	int Padding(char *ret,PaddingMode mode,int val);
-	void RemovePadding(FILE *fp_out,const char *buf,PaddingMode mode,int end_point);
-public:
-	AES(std::string key,unsigned int key_bit_length,const char *init_vec=NULL);
-	~AES(){}
-
-#ifdef USE_AES_NI
-	__m128i Encrypt(__m128i data);
-	__m128i Decrypt(__m128i data);
-	__m128i Encrypt_CBC(__m128i data,__m128i &vec);
-	__m128i Decrypt_CBC(__m128i data,__m128i &vec);
-#else
-	void Encrypt(unsigned char *data);
-	void Decrypt(unsigned char *data);
-	void Encrypt_CBC(unsigned char *data,unsigned char *vec);
-	void Decrypt_CBC(unsigned char *data,unsigned char *vec);
-#endif
-
-	Error_t Encrypt(std::string in_fname,std::string out_fname,bool cbc,PaddingMode mode);
-	Error_t Decrypt(std::string in_fname,std::string out_fname,bool cbc,PaddingMode mode);
+namespace aes {
+enum Mode {
+    AES_ECB,
+    AES_CBC_ZERO,
+    AES_CBC_PKCS5,
+    AES_CTR,
 };
+
+class Error {
+   public:
+    bool success;
+    std::string message;
+
+    Error() : success(true) {}
+    ~Error() {}
+};
+
+class AES {
+    static const unsigned int MAX_NR = 14;  // max no of rounds
+
+    unsigned int Nr;  // number of rounds
+    Error initError;
+    Mode mode;
+
+    // TODO(Padding, RemovePadding methods)
+    void Init(Mode mode, const unsigned char *key, unsigned int keyBitLen, unsigned char *iv);
+#if USE_AES_NI
+    __m128i encKey[MAX_NR + 2], decKey[MAX_NR + 2];
+    __m128i iv;  // initialization vector
+    __m128i AES_128_ASSIST(__m128i temp1, __m128i temp2);
+    void AES_192_ASSIST(__m128i &temp1, __m128i &temp2, __m128i &temp3);
+    void AES_256_ASSIST_1(__m128i &temp1, __m128i &temp2);
+    void AES_256_ASSIST_2(__m128i &temp1, __m128i &temp3);
+    void AES_128_Key_Expansion(__m128i *key, const unsigned char *userKey);
+    void AES_192_Key_Expansion(__m128i *key, const unsigned char *userKey);
+    void AES_256_Key_Expansion(__m128i *key, const unsigned char *userKey);
+
+    __m128i OneRoundEncrypt(__m128i data);
+    __m128i OneRoundDecrypt(__m128i data);
+#else
+    unsigned char iv[16];  // initialize vector
+    unsigned char roundKey[16 * (MAX_NR + 1)];
+
+    inline void ExtMul(unsigned char &x, unsigned char data, int n);
+    void SubWord(unsigned char *w);
+    void RotWord(unsigned char *w);
+    void KeyExpansion(const unsigned char *userKey, int wordkeyBitLength);
+
+    inline void SubBytes(unsigned char *data);
+    inline void ShiftRows(unsigned char *data);
+    inline void MixColumns(unsigned char *data);
+    inline void InvSubBytes(unsigned char *data);
+    inline void InvShiftRows(unsigned char *data);
+    inline void InvMixColumns(unsigned char *data);
+    inline void AddRoundKey(unsigned char *data, int n);
+
+    void OneRoundEncrypt(unsigned char *data);
+    void OneRoundDecrypt(unsigned char *data);
+#endif
+   public:
+    // generate initialize vector
+    static void GenerateIV(unsigned char *iv, Mode mode);
+
+    AES(const unsigned char *key, unsigned int keyBitLen);
+    AES(Mode mode, const unsigned char *key, unsigned int keyBitLen, unsigned char *iv);
+    ~AES() {}
+
+    Error Encrypt(std::string in_fname, std::string out_fname);
+    Error Encrypt(std::string in_message, std::string *out_message);
+    Error Decrypt(std::string in_fname, std::string out_fname);
+    Error Decrypt(std::string in_message, std::string *out_message);
+};
+};  // namespace aes
--- main.cpp
+++ main.cpp
@@ -1,146 +1,27 @@
-#ifndef _WINDOWS
-#if defined(_WIN64) || defined(_WIN32) || defined(__MINGW32__) || defined(__MINGW64__)
-#define _WINDOWS
-#endif
-#endif
-
-#define TIME_MEASUREMENT
-
-#include <iostream>
-#include <string.h>
-#include <stdlib.h>
 #include <stdio.h>
-#include <time.h>
 #include "aes.h"
-#include "option.h"
 
-#ifdef _WINDOWS
-#include <conio.h>
-#else
-#include <unistd.h>
-#endif
+int main() {
+    unsigned char iv[16];
+    unsigned char key[16] = {
+        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
+    };
+    std::string planeText = "secret texts";
+    std::string encMsg;
 
-using namespace std;
+    aes::AES::GenerateIV(iv, aes::AES_CTR);
+    aes::AES handler(aes::AES_CTR, key, 128, iv);
+    aes::Error err = handler.Encrypt(planeText, &encMsg);
+    if (!err.success) {
+        printf("Failed to encypt: %s\n", err.message.c_str());
+    }
 
-enum Mode{
-	MODE_NON,
-	MODE_ENCRYPT,
-	MODE_DECRYPT
-};
-
-void InputPassword(std::string &ret){
-#ifdef _WINDOWS
-	char c;
-	while((c=getch())!='\n'&&c!='\r'){
-		if(c=='\b'){// BackSpace
-			if(!ret.empty()){
-				printf("\b \b");
-				ret.erase(ret.size()-1);
-			}
-		}else if(c<0x20||c>0x7e){// 2byte character in Shift-JIS
-			getch();
-		}else if(c!='\t'&&c!=0x1b){// Tab,Esc?
-			putchar('*');
-			ret+=c;
-		}
-	}
-	putchar('\n');
-#else
-	ret=getpass("Password: ");
-#endif
-}
-
-int main(int argc,char *argv[]){
-	Option opt;
-	opt.AddHelpMessage("Usage: aes.exe (--enc or --dec) [options] input_file output_file");
-	opt.AddDefine("--cbc","","","if you want to encrypt(or decrypt) with cbc mode");
-	opt.AddDefine("--pad-zero","--pad-pkcs5","","select padding type(zero or PKCS#5, default is PKCS#5)");
-	opt.AddDefine("-l","--keylen","\"key_length\"","key bit length(128,192,256)");
-	opt.AddDefine("-p","--password","\"password\"","set password");
-	opt.AddDefine("-?","--help","","show this help message");
-	
-	if(!opt.SetArguments(argc,argv)){
-		opt.ShowHelpMessage();
-		return 1;
-	}
-	std::vector<std::string> opt_non=opt.GetOptionNon();
-	bool cbc=false;
-	Mode mode=MODE_NON;
-	AES::PaddingMode padding_mode=AES::PADDING_PKCS_5;
-	for(int i=0;i<opt_non.size();i++){
-		if(opt_non[i]=="--help"||opt_non[i]=="-?"){
-			opt.ShowHelpMessage();
-			return 0;
-		}else if(opt_non[i]=="--cbc")
-			cbc=true;
-		else if(opt_non[i]=="--enc"){
-			if(mode==MODE_NON)
-				mode=MODE_ENCRYPT;
-			else{
-				cerr<<"Please input --enc or --dec"<<endl;
-				exit(1);
-			}
-		}else if(opt_non[i]=="--dec"){
-			if(mode==MODE_NON)
-				mode=MODE_DECRYPT;
-			else{
-				cerr<<"Please input --enc or --dec";
-				exit(1);
-			}
-		}else if(opt_non[i]=="--pad-zero")
-			padding_mode=AES::PADDING_ZERO;
-		else if(opt_non[i]=="--pad-pkcs5")
-			padding_mode=AES::PADDING_PKCS_5;
-		else{
-			opt.ShowHelpMessage();
-			return 1;
-		}
-	}
-	if(mode==MODE_NON){
-		opt.ShowHelpMessage();
-		return 1;
-	}
-
-	int key_length=128;
-	std::string password;
-	std::map<std::string,std::string> opt_val=opt.GetOptionValue();
-	for(map<string,string>::iterator it=opt_val.begin();it!=opt_val.end();it++){
-		if(it->first=="-l"||it->first=="--keylen")
-			key_length=atoi(it->second.c_str());
-		else if(it->first=="-p"||it->first=="--password")
-			password=it->second;
-	}
-	std::vector<std::string> input=opt.GetInput();
-	if(input.size()!=2){
-		opt.ShowHelpMessage();
-		return 1;
-	}
-
-	if(password.empty()){
-		cout<<"Please Input Password"<<endl;
-		InputPassword(password);
-	}
-
-	if(password.empty()){
-		cout<<"This system is not allow [encryption|decryption] with empty password."<<endl;
-		return 0;
-	}
-
-#ifdef TIME_MEASUREMENT
-	clock_t start_time=clock();
-#endif
-	AES aes(password,key_length);
-	switch(mode){
-	case MODE_ENCRYPT:
-		aes.Encrypt(input[0],input[1],cbc,padding_mode);
-		break;
-	case MODE_DECRYPT:
-		aes.Decrypt(input[0],input[1],cbc,padding_mode);
-		break;
-	}
-#ifdef TIME_MEASUREMENT
-	clock_t end_time=clock();
-	printf("%.2f[sec]\n",(double)(end_time-start_time)/CLOCKS_PER_SEC);
-#endif
-	return 0;
+    printf("iv: (");
+    for (int i = 0; i < 16; i++) {
+        printf("%d", iv[i]);
+        if (i < 15) {
+            printf(", ");
+        }
+    }
+    puts(")");
 }
