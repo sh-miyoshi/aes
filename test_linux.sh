ddd99d07423a221d5074e6798915bc0fd8e47bbf
s.miyoshi <s.miyoshi@jp.fujitsu.com>
2019-10-17 10:32:53 +0900

    add test case for linux

--- test_linux.sh
+++ test_linux.sh
@@ -1,11 +1,47 @@
 #!/bin/bash
 
+echo "--- initialize process ---"
 mkdir -p tmp
 make
+echo "--- finished initlization ---"
 
-./aes --enc -p test README.md tmp/encrypt.enc
-./aes --dec -p test tmp/encrypt.enc tmp/result.md
-
-echo "---diff data---"
+echo "test 128-bit, CTR"
+./aes --enc -p test -m ctr -l 128 README.md tmp/encrypt.enc
+./aes --dec -p test -m ctr -l 128 tmp/encrypt.enc tmp/result.md
 diff README.md tmp/result.md
-echo "---diff end---"
+
+echo "test 192-bit, CTR"
+./aes --enc -p test -m ctr -l 192 README.md tmp/encrypt.enc
+./aes --dec -p test -m ctr -l 192 tmp/encrypt.enc tmp/result.md
+diff README.md tmp/result.md
+
+echo "test 256-bit, CTR"
+./aes --enc -p test -m ctr -l 256 README.md tmp/encrypt.enc
+./aes --dec -p test -m ctr -l 256 tmp/encrypt.enc tmp/result.md
+diff README.md tmp/result.md
+
+
+echo "test 128-bit, CBC, PKCS#5"
+./aes --enc -p test -m cbc -l 128 --padding pkcs5 README.md tmp/encrypt.enc
+./aes --dec -p test -m cbc -l 128 --padding pkcs5 tmp/encrypt.enc tmp/result.md
+diff README.md tmp/result.md
+
+echo "test 192-bit, CBC, PKCS#5"
+./aes --enc -p test -m cbc -l 192 --padding pkcs5 README.md tmp/encrypt.enc
+./aes --dec -p test -m cbc -l 192 --padding pkcs5 tmp/encrypt.enc tmp/result.md
+diff README.md tmp/result.md
+
+echo "test 256-bit, CBC, PKCS#5"
+./aes --enc -p test -m cbc -l 256 --padding pkcs5 README.md tmp/encrypt.enc
+./aes --dec -p test -m cbc -l 256 --padding pkcs5 tmp/encrypt.enc tmp/result.md
+diff README.md tmp/result.md
+
+echo "test 256-bit, CBC, ZERO"
+./aes --enc -p test -m cbc -l 256 --padding zero README.md tmp/encrypt.enc
+./aes --dec -p test -m cbc -l 256 --padding zero tmp/encrypt.enc tmp/result.md
+diff README.md tmp/result.md
+
+echo "test 256-bit, ECB, PKCS#5"
+./aes --enc -p test -m ecb -l 256 --padding pkcs5 README.md tmp/encrypt.enc
+./aes --dec -p test -m ecb -l 256 --padding pkcs5 tmp/encrypt.enc tmp/result.md
+diff README.md tmp/result.md
